2c47e1dd25ef5ea09de1c904ad4971713fe745dc
sampath surineni <ssurineni@salesforce.com>
2023-08-10 12:27:50 +0530

    rgs2.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,26 @@
 
 import React from 'react';
 
- class App extends React.Component {
+class Card extends React.Component {
     render() {
-        return <div className="header">{this.props.title}</div>;
+        return (
+            <div className="github-profile" >
+                <img src="https://placehold.it/75" />
+                <div className="info">
+                    <div className="name">Name here...</div>
+                    <div className="company">Company here..</div>
+                </div>
+            </div >
+        );
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (<div>
+            <div className="header">{this.props.title}</div>
+            <Card />
+        </div>);
     }
 }
 
