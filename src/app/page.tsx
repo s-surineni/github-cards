cc2406850ed1fbffbfa370b8aad91a2ba2c1c5c6
sampath surineni <ssurineni@salesforce.com>
2023-08-14 17:46:37 +0530

    rgs2.4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,24 @@ const testData = [
     {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
 ];
 
+class Form extends React.Component {
+    state = { userName: '' };
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.userName);
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" value={this.state.userName} onChange={event => this.setState({ userName: event.target.value })} 
+                placeholder='Github username'
+                required/>
+                <button>Add card</button>
+            </form>
+        )
+    }
+}
+
 
 class Card extends React.Component {
     render() {
@@ -38,6 +56,7 @@ class App extends React.Component {
     render() {
         return (<div>
             <div className="header">{this.props.title}</div>
+            <Form />
             <CardList />
         </div>);
     }
